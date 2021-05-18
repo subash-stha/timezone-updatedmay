4f14b7a3e760d7b96e4189c126f5635d0bfed9a8
subas48 <subas48@gmail.com>
2021-05-18 12:19:31 +0545

    Remove commented code

--- src/components/TimeDiff.vue
+++ src/components/TimeDiff.vue
@@ -51,14 +51,6 @@ export default {
     }
     },
     methods: {
-      // changeFirstTimeZone (event) {
-      //   this.selectedZoneFirst = event.target.options[event.target.options.selectedIndex].text
-      // },
-      // changeSecondTimeZone (event) {
-      //   this.selectedZoneSecond = event.target.options[event.target.options.selectedIndex].text
-      //   var d = new Date()
-      //   this.secondTime = d.toLocaleString('en-CA', { timezone: this.selectedZoneSecond, hour12: false }).replace(', ', 'T');
-      // },
       checkDifference() {
         fetch(`http://api.timezonedb.com/v2.1/convert-time-zone?key=YXSTQ46QNFJU&format=json&from=${this.firstTimezone}&to=${this.secondTimezone}
 `).then(res => res.json()).then(response => {
@@ -67,24 +59,7 @@ export default {
         const diff = new Date(seconds * 1000).toISOString().substr(11,8);
         this.timeDifference = diff;
         console.log('response', diff);
-        // this.zones = response.zones
       })
-        // var date = new Date()
-        // var timeZone = this.selectedZoneFirst;
-        // var timeZone2 = this.selectedZoneSecond;
-
-        // let updatedTimeZone1 = date.toLocaleString('en-CA', { timeZone, hour12: false }).replace(', ', 'T');
-        // let updatedTimeZone2 = date.toLocaleString('en-CA', { timeZone2, hour12: false }).replace(', ', 'T');
-        // updatedTimeZone1 += '.' + date.getMilliseconds().toString().padStart(3, '0');
-        // updatedTimeZone2 += '.' + date.getMilliseconds().toString().padStart(3, '0');
-        // const newTime1 = new Date(updatedTimeZone1 + 'Z');
-        // const newTime2 = new Date(updatedTimeZone2 + 'Z');
-        // this.firstTime = -(newTime1 - date) / 60 / 1000;
-        // this.secondTime = -(newTime2 - date) / 60 / 1000;
-        // var diff = this.firstTime - this.secondTime
-        // var hours = Math.floor(diff / 60);          
-        // var minutes = diff % 60;
-        // this.finalDiff = hours ? hours + " " + 'hours and' + " " + minutes + " " + "minutes" : minutes ? minutes + " " + 'minutes' : '' 
       }
     },
     mounted() {
